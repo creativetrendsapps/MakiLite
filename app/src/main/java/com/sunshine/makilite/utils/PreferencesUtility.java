ec59708433e216b95b7b2075cfea2ec527de4d1b
sfilmak <savas.savchin@gmail.com>
2017-02-19 20:25:30 +0200

    Update PreferencesUtility.java

--- app/src/main/java/com/sunshine/makilite/utils/PreferencesUtility.java
+++ app/src/main/java/com/sunshine/makilite/utils/PreferencesUtility.java
@@ -11,7 +11,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  */
-
+ 
 package com.sunshine.makilite.utils;
 
 import android.content.Context;
